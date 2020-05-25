adbd6a41eccbf7297a01c252770e2abfd2bc4108
Trend20 <enockomondi305@gmail.com>
2020-05-25 15:24:59 -0400

    quoteauthor

---
+++ src/components/QuoteAuthor.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+ 
+class QuoteAuthor extends Component {
+    render() { 
+        return (
+            <div>
+                QuoteAuthor
+            </div>
+        );
+    }
+}
+ 
+export default QuoteAuthor;
