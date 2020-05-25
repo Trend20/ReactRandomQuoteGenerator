a7f1e8ab4ded11503a7d989a01019942ba22610a
Trend20 <enockomondi305@gmail.com>
2020-05-25 15:28:44 -0400

    database

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import Quotes from './components/QuotesDatabase';
+import QuoteAuthor from './components/QuoteAuthor';
 import './App.css'
  
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      quote: quotes[0].quote,
+      author: quotes[0].author,
+    }
+  }
   render() { 
     return (
       <div>
-        App
+        <QuoteAuthor />
+        <Quotes />
       </div>
     );
   }
