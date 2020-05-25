9af2e38cdc4dbc39c9fac2babd67842da1ee5565
Trend20 <enockomondi305@gmail.com>
2020-05-25 15:21:11 -0400

    app

--- src/App.js
+++ src/App.js
@@ -1,58 +1,11 @@
 import React, { Component } from 'react';
  
 class App extends Component {
-  state={
-    list:[],
-    newQuote:[
-      {
-        "quote":'hey love',
-        "author": 'dimore'
-      },
-      {
-        "quote":'hey mum',
-        "author": 'stevo'
-      },
-      {
-        "quote":'hey dad',
-        "author": 'eriko'
-      },
-      {
-        "quote":'hey siz',
-        "author": 'wifi'
-      }
-    ],
-  }
-  addQuote(){
-    const newQuote={
-      id: 1 + Math.random(),
-      value: this.state.newQuote.quote,
-    }
-
-    const list=[...this.state.list];
-
-    list.push(JSON.parse(newQuote));
-
-    this.setState({
-      list,
-      newQuote:'',
-    });
-  }
   render() { 
     return (
-      <div className="App">
-      <div className="quote-container">
-        {
-          this.state.list.map(quote =>{
-            return(
-              <p key={quote.id}>
-                {quote.value}
-              </p>
-            )
-          })
-        }
-        <button onClick={() =>this.addQuote()}>New Quote</button>
+      <div>
+        App
       </div>
-    </div>
     );
   }
 }
