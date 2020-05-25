e10ae26e46eac057c7c12faad99e346014cc5bc6
Trend20 <enockomondi305@gmail.com>
2020-05-25 15:30:10 -0400

    another quote

--- src/App.js
+++ src/App.js
@@ -11,6 +11,31 @@ class App extends Component {
       author: quotes[0].author,
     }
   }
+  randomQuote() {
+    const randomNumber = Math.floor(Math.random() * quotes.length);
+    return quotes[randomNumber];
+    
+  }
+  shuffleQuotes(array){
+    return array.sort(()=>Math.random()-0.5)
+  }
+
+  handleClick = () => {
+    const generateRandomQuote = this.randomQuote();
+    this.setState({
+      quote: generateRandomQuote.quote,
+      author: generateRandomQuote.author
+    });
+    this.shuffleQuotes(quotes)
+  };
+
+  randomColor() {
+    const color = `rgb(
+      ${Math.floor(Math.random() * 155)},
+      ${Math.floor(Math.random() * 155)},
+      ${Math.floor(Math.random() * 155)})`;
+    return color;
+  }
   render() { 
     return (
       <div>
