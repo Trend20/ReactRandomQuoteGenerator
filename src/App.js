ac618eafdb6fc4e69db18d584a7eb8cda99a24ff
Trend20 <enockomondi305@gmail.com>
2021-01-25 09:50:45 +0300

    changing the text font

--- src/App.js
+++ src/App.js
@@ -2,51 +2,50 @@ import React, { Component } from 'react';
 import quotes from './components/QuotesDatabase';
 import QuoteAuthor from './components/QuoteAuthor';
 import './App.css'
- 
+
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      quote: quotes[0].quote,
-      author: quotes[0].author,
+    constructor(props) {
+        super(props);
+        this.state = {
+            quote: quotes[0].quote,
+            author: quotes[0].author,
+        }
     }
-  }
-  randomQuote() {
-    const randomNumber = Math.floor(Math.random() * quotes.length);
-    return quotes[randomNumber];
-    
-  }
-  shuffleQuotes(array){
-    return array.sort(()=>Math.random()-0.5)
-  }
+    randomQuote() {
+        const randomNumber = Math.floor(Math.random() * quotes.length);
+        return quotes[randomNumber];
 
-  handleClick = () => {
-    const generateRandomQuote = this.randomQuote();
-    this.setState({
-      quote: generateRandomQuote.quote,
-      author: generateRandomQuote.author
-    });
-    this.shuffleQuotes(quotes)
-  };
+    }
+    shuffleQuotes(array) {
+        return array.sort(() => Math.random() - 0.5)
+    }
 
-  randomColor() {
-    const color = `rgb(
+    handleClick = () => {
+        const generateRandomQuote = this.randomQuote();
+        this.setState({
+            quote: generateRandomQuote.quote,
+            author: generateRandomQuote.author
+        });
+        this.shuffleQuotes(quotes)
+    };
+
+    randomColor() {
+        const color = `rgb(
       ${Math.floor(Math.random() * 155)},
       ${Math.floor(Math.random() * 155)},
       ${Math.floor(Math.random() * 155)})`;
-    return color;
-  }
-  render() { 
-    return (
-      <div>
-        <QuoteAuthor 
-        displayColor={this.randomColor}
-          handleClick={this.handleClick}
-          {...this.state}
-        />
-      </div>
-    );
-  }
+        return color;
+    }
+    render() {
+        return ( <
+            div className = "app" >
+            <
+            QuoteAuthor displayColor = { this.randomColor }
+            handleClick = { this.handleClick } {...this.state }
+            /> < /
+            div >
+        );
+    }
 }
- 
+
 export default App;
